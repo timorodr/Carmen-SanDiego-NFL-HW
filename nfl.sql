17bc4cf1b63b2cf11d50b7a5639dc6cf77137555
Timothy Rodriguez <tim.rdrgz2017@live.com>
2024-03-24 21:35:49 -0500

    NFL queries 2

--- nfl.sql
+++ nfl.sql
@@ -2,3 +2,8 @@
 -- 2. SELECT stadium, head_coach FROM teams;
 -- 3. SELECT head_coach FROM teams WHERE conference = 'AFC' AND division = 'South';
 -- 4. SELECT MAX(id) FROM players; 
+-- 5. SELECT name, head_coach FROM teams WHERE conference = 'NFC' AND division = 'North';
+-- 5. SELECT name, head_coach FROM teams WHERE conference = 'AFC' AND division = 'East';
+-- 6. SELECT salary, name FROM players ORDER BY salary DESC LIMIT 50;
+-- 7. SELECT AVG(salary) FROM players;
+-- 8. SELECT name, position, salary FROM players WHERE salary >= 10000000;
